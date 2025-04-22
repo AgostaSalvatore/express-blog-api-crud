9d4df7e903263a6da3e446aa1be38244cc25f6d0
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-22 14:22:59 +0200

    [BONUS DONE] added control on show and idex

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,19 +3,41 @@ const posts = require('../data/posts.js');
 
 //index
 function index(req,res){
-    res.json(posts);
+    //res.json(posts);
+
+    //filtraggio: uso delle query string
+    const postTitle = req.query.title;
+
+    //definizione della varibila che contiene il post filtrato
+    let filteredPost = posts;
+
+    //controllo
+    if(postTitle){
+        filteredPost = posts.filter((post) =>{
+            return post.title.toLowerCase().includes(postTitle.toLowerCase());
+        })
+    }
+
+    res.json(filteredPost);
 };
 
 //show
 function show(req,res){
+    //recupero l'id del post
     const id = parseInt(req.params.id);
+
+    //
     const post = posts.find(p => p.id === id);
+
+    //controllo
     if (post) {
         res.json(post);
     }
     else{
         //importo lo stato
         res.status(404);
+
+        //messaggio di ritorno per l'utente
         return res.json({
             message: `posts numero ${req.params.id} non trovato`
         });
