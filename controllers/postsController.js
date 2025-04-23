0d0be5907186c355db49bfdac2526d9f9647c903
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-23 13:53:21 +0200

    [UPD/DONE] done + bonus

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -58,14 +58,17 @@ function store(req,res){
     
     //definizione dell'id dell'elemento da inserire
     const newId = posts[posts.length - 1].id + 1;
+
+    //destructuring
+    const {title, content, image, tags} = req.body;
     
     //creazione dell'oggetto da inserire
     const newPost = {
         id: newId,
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        tags: req.body.tags
+        title: title,
+        content: content,
+        image: image,
+        tags: tags
     };
     
     //inserimento dell'oggetto nell'array
@@ -86,14 +89,27 @@ function update(req,res){
     //vado a recuperare il post
     const post = posts.find(post => post.id === id);
 
-    //recupero il body della richiesta
-    console.log(req.body);
-    
+    //controllo che il post esista
+    if(!post){
+
+        //importo lo stato
+        res.status(404);
+
+        //messaggio di ritorno per l'utente
+        return res.json({
+            message: `Post numero ${req.params.id} non trovato`
+        });
+    }
+
+
+    //destructuring
+    const {title, content, image, tags} = req.body;
+
     //modifico il post
-    post.title = req.body.title;
-    post.content = req.body.content;
-    post.image = req.body.image;
-    post.tags = req.body.tags;
+    post.title = title;
+    post.content = content;
+    post.image = image;
+    post.tags = tags;
 
     //restituisco il post modificato
     res.json(post);
